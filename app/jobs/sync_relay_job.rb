bba62f4f9a9345f70d57e43d51f4b710840b6fd3
David T. Allen <148455+bitlather@users.noreply.github.com>
2020-07-29 16:17:45 -0400

    Wait 3 seconds then return the message again

--- app/jobs/sync_relay_job.rb
+++ app/jobs/sync_relay_job.rb
@@ -3,5 +3,8 @@ class SyncRelayJob < ApplicationJob
   def perform(sync) #*DTA should be sync, and other stuff should be changed too
     ActionCable.server.broadcast "integration_name:#{sync.user_id.to_i}:sync",
       sync: SyncsController.render(partial: 'syncs/sync', locals: { sync: sync })
+    sleep 3
+    ActionCable.server.broadcast "integration_name:#{sync.user_id.to_i}:sync",
+      sync: SyncsController.render(partial: 'syncs/sync', locals: { sync: sync })
   end
 end
